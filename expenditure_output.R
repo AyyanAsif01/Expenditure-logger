b82088295e077ba046a1e19c099295e8f5f34c3d
Ayyan Asif <154856552+AyyanAsif01@users.noreply.github.com>
2023-12-28 00:52:56 -0700

    Add files via upload

---
+++ expenditure_output.R
@@ -0,0 +1,34 @@
+Options:
+  1. Input Expense
+2. Show Total Expenditure
+3. Show Average Monthly Expenses
+4. Exit
+
+Enter your choice: 1
+Enter the date (yyyy-mm-dd): 2023-03-01
+Enter the amount: 100
+Enter the category: Groceries
+
+Enter your choice: 1
+Enter the date (yyyy-mm-dd): 2023-03-15
+Enter the amount: 50
+Enter the category: Utilities
+
+Enter your choice: 1
+Enter the date (yyyy-mm-dd): 2023-04-10
+Enter the amount: 75
+Enter the category: Entertainment
+
+Enter your choice: 2
+Total Expenditure: $225
+
+Enter your choice: 3
+Average Monthly Expenses: $112.50
+
+Enter your choice: 4
+Exiting the program...
+
+
+
+
+
